8d737a8c47855833b50f10e4cdb11b72e389ff6e
Ilya Stepaniuk <ilya.stepaniuk@patrianna.com>
2023-11-11 18:43:15 +0200

    add project

---
+++ index.js
@@ -0,0 +1,28 @@
+import express from "express"
+
+const app = express()
+const PORT = 3000
+
+app.listen(PORT, () => {
+  console.log(`API listening on PORT ${PORT} `)
+})
+
+app.get("/", async (req, res) => {
+  try {
+    const fetchOptions = {
+      headers: {
+        apiKey: process.env.API_KEY,
+      },
+    }
+
+    const fetchResponse = await fetch(process.env.URL, fetchOptions)
+    const courses = await fetchResponse.json()
+
+    res.json({ courses })
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Internal Server Error")
+  }
+})
+
+module.exports = app
